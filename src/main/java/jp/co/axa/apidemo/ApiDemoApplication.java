83a1d69219c45f474d2ff85380756c979aab5f88
Tridev2022 <me.tridev21@gmail.com>
2023-06-14 11:31:44 +0900

    Commit for initial setup

---
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -0,0 +1,19 @@
+package jp.co.axa.apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author user
+ *
+ */
+@EnableSwagger2
+@SpringBootApplication
+public class ApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemoApplication.class, args);
+	}
+
+}
